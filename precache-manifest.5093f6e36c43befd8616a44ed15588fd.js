6952336a42d4655992f204213490af9f98db683b
Nikita Tezyakov <nikitez37@gmail.com>
2026-06-21 15:48:17 +0300

    Updates

---
+++ precache-manifest.5093f6e36c43befd8616a44ed15588fd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3f804fbf68dcbb57b7e793d57882d5",
+    "url": "/ravplayer/index.html"
+  },
+  {
+    "revision": "77ae4f24bd576c4eac98",
+    "url": "/ravplayer/static/css/main.1478d368.chunk.css"
+  },
+  {
+    "revision": "d444848f5d780f2c5091",
+    "url": "/ravplayer/static/js/2.79615572.chunk.js"
+  },
+  {
+    "revision": "77ae4f24bd576c4eac98",
+    "url": "/ravplayer/static/js/main.a8b13d95.chunk.js"
+  },
+  {
+    "revision": "9e2e22480ae07a86927f",
+    "url": "/ravplayer/static/js/runtime~main.0daa744d.js"
+  },
+  {
+    "revision": "66bbee01a2a47fe02e7a0504380b3388",
+    "url": "/ravplayer/static/media/close.66bbee01.svg"
+  },
+  {
+    "revision": "039c4203894ff35fc53479922d3065f5",
+    "url": "/ravplayer/static/media/closeWhite.039c4203.svg"
+  },
+  {
+    "revision": "feeb8178e0ca04c3260bb23902a415c2",
+    "url": "/ravplayer/static/media/drum.feeb8178.svg"
+  },
+  {
+    "revision": "dc175704886b40888dc56226dd313e19",
+    "url": "/ravplayer/static/media/drum10.dc175704.png"
+  },
+  {
+    "revision": "c6fd060c1ec3f07806dd16a23c0507ac",
+    "url": "/ravplayer/static/media/drum11.c6fd060c.svg"
+  },
+  {
+    "revision": "080661a8687aedd1f234914e6275647a",
+    "url": "/ravplayer/static/media/drum12.080661a8.svg"
+  },
+  {
+    "revision": "bb8d260b796499c1c4fde9b0da7c60a5",
+    "url": "/ravplayer/static/media/drum13.bb8d260b.svg"
+  },
+  {
+    "revision": "9c4d69decc6c7ede43625197c1e135a1",
+    "url": "/ravplayer/static/media/drum14.9c4d69de.png"
+  },
+  {
+    "revision": "f9574ae3114fe1c03fdf5645010dc531",
+    "url": "/ravplayer/static/media/drum15.f9574ae3.png"
+  },
+  {
+    "revision": "f2d2b85fb26e47b44fcc9ec4bafea9b2",
+    "url": "/ravplayer/static/media/drum16.f2d2b85f.png"
+  },
+  {
+    "revision": "1778eb41443c1b2b27325791d0e06d48",
+    "url": "/ravplayer/static/media/drum9.1778eb41.png"
+  },
+  {
+    "revision": "330783b999cdce516c01d5e3762a993f",
+    "url": "/ravplayer/static/media/drum9P.330783b9.svg"
+  },
+  {
+    "revision": "913838c32e95a2421ecea3eb09864854",
+    "url": "/ravplayer/static/media/drumsticks.913838c3.svg"
+  },
+  {
+    "revision": "e98a9425647c38f67dbbe0598ad843d6",
+    "url": "/ravplayer/static/media/finger.e98a9425.svg"
+  },
+  {
+    "revision": "2814ffdb1e6f792e9e7621f068dfdea8",
+    "url": "/ravplayer/static/media/moon.2814ffdb.svg"
+  },
+  {
+    "revision": "bd566d5c9f62d84b1a2365e5f05ace36",
+    "url": "/ravplayer/static/media/more.bd566d5c.svg"
+  },
+  {
+    "revision": "c3854dc2142471b307adf5e6cc5c38b2",
+    "url": "/ravplayer/static/media/mystic.c3854dc2.svg"
+  },
+  {
+    "revision": "2bd17eb5890026e56283e02136cd3f5a",
+    "url": "/ravplayer/static/media/pan.2bd17eb5.svg"
+  },
+  {
+    "revision": "28bef82ed8bcdf1b18d76b8535e2cf09",
+    "url": "/ravplayer/static/media/play.28bef82e.svg"
+  },
+  {
+    "revision": "0716b53414c2b28901f48d6f7c5e4810",
+    "url": "/ravplayer/static/media/stop.0716b534.svg"
+  }
+]);
